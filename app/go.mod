1212525d56c45d875fb29b92da41b2d7ce1a08b0
SakaiTaka23 <tt3.sakai@gmail.com>
2020-10-28 13:13:42 +0900

    db接続の方法を変更

--- app/go.mod
+++ app/go.mod
@@ -3,6 +3,7 @@ module app
 go 1.15
 
 require (
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
--- app/main.go
+++ app/main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "user:password@tcp(127.0.0.1:3306)/blog_system_echo?parseTime=True"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	err := godotenv.Load(fmt.Sprintf("../%s.env", os.Getenv("GO_ENV")))
 	if err != nil {
 		fmt.Print(err)
 	}
+
+	DBConnect()
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
@@ -22,4 +25,17 @@ func main() {
 	})
 	e.Logger.Fatal(e.Start(":8082"))
 	// e.Startの中はdocker-composeのgoコンテナで設定したportsを指定してください。
+}
+
+func DBConnect() *gorm.DB {
+	USER := os.Getenv("DB_USER")
+	PASS := os.Getenv("DB_PASS")
+	PROTOCOL := "tcp(127.0.0.1:3306)"
+	DBNAME := os.Getenv("DB")
+	dsn := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
 }
